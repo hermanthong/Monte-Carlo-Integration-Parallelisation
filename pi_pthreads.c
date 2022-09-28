ef7d963ffee265b9e4ab58cd11243825faf430b6
Tharen Candi <tcan7909@uni.sydney.edu.au>
2022-09-28 16:32:01 +0200

    task 2:

--- pi_pthreads.c
+++ pi_pthreads.c
@@ -9,6 +9,7 @@ SCIPER		: Your SCIPER numbers
 #include <stdio.h>
 #include <stdlib.h>
 #include "utility.h"
+#include <pthread.h>
 
 double calculate_pi (int num_threads, int samples);
 
@@ -33,11 +34,49 @@ int main (int argc, const char *argv[]) {
     return 0;
 }
 
+typedef struct {
+    size_t id;
+    int samples;
+    int result;
+} thread_args;
+
+void* thread_worker(void * args) {
+    thread_args *t_args = (thread_args*) args;
+    struct rand_gen rand = init_rand_pthreads(t_args->id);
+    float randX, randY = 0.0;
+    for (int i = 0; i < t_args->samples; ++i) {
+            randX = next_rand(rand);
+            randY = next_rand(rand);
+
+            //0 to 1 inclusive
+            if ((randX * randX) + (randY * randY) < 1)
+                ++t_args->result;
+        }
+
+    free_rand(rand);
+    return NULL;
+}
 
 double calculate_pi (int num_threads, int samples) {
     double pi;
+    pthread_t tid[num_threads];
+    thread_args *args = malloc(sizeof(thread_args) * num_threads);
+    int insideCircle = 0;
+    //create the threads and make them work
+    for (int i = 0; i < num_threads; i ++) {
+        args[i].samples = samples/num_threads;
+        args[i].id = tid[i];
+        args[i].result = 0;
+        pthread_create(&tid[i], NULL, thread_worker, (void*)&args[i]);
+    }
 
-    /* Your code goes here */
-
+    //join the threads and get final summation
+    for (int i = 0; i < num_threads; i ++) {
+        pthread_join(tid[i], NULL);
+        insideCircle += args[i].result;
+    }
+    
+    free(args);
+    pi = 4.0 * insideCircle / samples;
     return pi;
 }
