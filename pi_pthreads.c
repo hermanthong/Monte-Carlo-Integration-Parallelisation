9890888fb013908ee688ecbe968eb4211689d455
Tharen Candi <tcan7909@uni.sydney.edu.au>
2022-10-09 15:08:01 +0200

    fix pthreads

--- pi_pthreads.c
+++ pi_pthreads.c
@@ -52,7 +52,7 @@ void* thread_worker(void * args) {
             if ((randX * randX) + (randY * randY) < 1)
                 ++t_args->result;
         }
-
+    
     free_rand(rand);
     return NULL;
 }
@@ -60,23 +60,26 @@ void* thread_worker(void * args) {
 double calculate_pi (int num_threads, int samples) {
     double pi;
     pthread_t tid[num_threads];
-    thread_args *args = malloc(sizeof(thread_args) * num_threads);
+    thread_args **args = malloc(sizeof(thread_args*) * num_threads);
     int insideCircle = 0;
     //create the threads and make them work
     for (int i = 0; i < num_threads; i ++) {
-        args[i].samples = samples/num_threads;
-        args[i].id = tid[i];
-        args[i].result = 0;
-        pthread_create(&tid[i], NULL, thread_worker, (void*)&args[i]);
+        args[i] = malloc(sizeof(thread_args));
+
+        args[i]->samples = samples/num_threads;
+        args[i]->id = tid[i];
+        args[i]->result = 0;
+        pthread_create(&tid[i], NULL, thread_worker, (void*)args[i]);
     }
 
     //join the threads and get final summation
     for (int i = 0; i < num_threads; i ++) {
         pthread_join(tid[i], NULL);
-        insideCircle += args[i].result;
+        insideCircle += args[i]->result;
+        free(args[i]);
     }
-    
     free(args);
+    //free(args);
     pi = 4.0 * insideCircle / samples;
     return pi;
 }
